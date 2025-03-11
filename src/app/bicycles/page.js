c8745ab9470e8004eec16037e0b6d770e26f7cd2
Paulo Silva <paulofernando78@gmail.com>
2025-03-11 01:39:12 -0300

    wip

--- src/app/bicycles/page.js
+++ src/app/bicycles/page.js
@@ -15,9 +15,9 @@ export default function Bicycles() {
       <h2 className="margin-bottom">Explore our models</h2>
       <div className={styles["bicycle-container"]}>
         <div className={`line-break ${styles["card"]}`}>
-          <Image src={velocityV3} alt="Velocity Z2" layout="intrinsic" />
+          <Image src={velocityV3} alt="Velocity Z2" layout="intrinsic" className={styles["image"]}/>
           <h3>Velocity Z2</h3>
-          <p>
+          <p className={styles["paragraph"]}>
             The Velocity Z3 is a high-speed machine built for racing and extreme
             performance. With an aerodynamic frame, ultra-light materials, and
             cutting-edge battery technology, this bike delivers lightning-fast
@@ -26,9 +26,9 @@ export default function Bicycles() {
           </p>
         </div>
         <div className={`line-break ${styles["card"]}`}>
-          <Image src={vortexV9} alt="Vortex V9" layout="intrinsic" />
+          <Image src={vortexV9} alt="Vortex V9" layout="intrinsic" className={styles["image"]}/>
           <h3>Vortex V9</h3>
-          <p>
+          <p className={styles["paragraph"]}>
             The Vortex V9 is designed for effortless urban commuting with a
             focus on comfort and pleasure. Its smooth ride, ergonomic design,
             and intelligent assistance features make it the perfect choice for
@@ -37,9 +37,9 @@ export default function Bicycles() {
           </p>
         </div>
         <div className={`line-break ${styles["card"]}`}>
-          <Image src={quantumX1} alt="Quantum X1" layout="intrinsic" />
+          <Image src={quantumX1} alt="Quantum X1" layout="intrinsic" className={styles["image"]}/>
           <h3>Quantum X1</h3>
-          <p>
+          <p className={styles["paragraph"]}>
             The Quantum X1 blends high-speed performance with urban comfort.
             Featuring a sleek frame and advanced battery system for swift
             acceleration and long-range rides, it offers intelligent assistance
