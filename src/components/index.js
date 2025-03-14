4461ca2e6cd25fa2a75d78a8837122b6cfe58c37
Paulo Silva <paulofernando78@gmail.com>
2025-03-13 22:33:39 -0300

    wip

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import { Header } from "./Header/Header";
+import { NavBar } from "./NavBar/NavBar";
+import { Card } from "./Card/Card";
+import { Footer } from "./Footer/Footer";
+
+export { Header, NavBar, Card, Footer };
