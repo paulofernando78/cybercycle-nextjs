730835ee26f7abe9abe34f6999046d2173a07a11
Paulo Silva <paulofernando78@gmail.com>
2025-03-13 21:53:25 -0300

    wip

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import styles from './Footer.module.css'
 
 export const Footer = () => {
   return (
-    <div className={styles["footer"]}>
+    <div className={styles["footer__container"]}>
       <div>Contact</div>
       <div>Information</div>
       <div>Social</div>
